d5d502d3098b98a98e772df8e364e090aeebf3d1
NathanOrme <nathan.orme23@gmail.com>
2016-05-26 16:17:35 +0100

    moved all the junit to new package

---
+++ test/junitclasses/UserControllerTest.java
@@ -0,0 +1,45 @@
+package junitclasses;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.qa.ims.App;
+import com.qa.ims.controller.UserController;
+import com.qa.ims.model.UserModel;
+import com.qa.ims.model.repository.UserModelRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = App.class)
+public class UserControllerTest {
+	
+	@Autowired
+	private UserController userController;
+	@Autowired
+	private UserModelRepository usermodelRepository;
+	
+	private UserModel user;
+	
+	@Before
+	public void setUp(){
+		user = new UserModel("Sam", "Chicken");
+		usermodelRepository.save(user);
+		
+	}
+	
+	@After
+	public void cleanUp(){
+		usermodelRepository.deleteAll();
+	}
+
+	@Test
+	public void testAuthenticate() {
+		userController.authenticate(user.getUsername(), user.getPassword());
+	}
+
+}
